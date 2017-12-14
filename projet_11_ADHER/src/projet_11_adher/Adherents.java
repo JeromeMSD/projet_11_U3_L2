ddc4b73c5e7484a9f8db70c75e1820ac862960cd
romain <romain@kali>
2017-12-14 13:55:16 +0100

    j'ai fait des classes

---
+++ projet_11_ADHER/src/projet_11_adher/Adherents.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projet_11_adher;
+
+/**
+ *
+ * @author romain
+ */
+public class Adherents {
+    private [String][nom];
+    private [String][prenom];
+    private [String][adresse];
+    private [Date][dateEntree];
+    private [Date][dateSortie];
+    private [Boolean][disponibilite]    
+}
+[Void][adherents]([String nom],[String prenom], [String adresse], [Date dateEntree], [Date dateSortie]){
+
+;
+}
+
