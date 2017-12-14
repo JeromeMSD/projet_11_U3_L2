c46eeb6c913a63c8bbfbcca0c06014404d23f150
jmddu <jmddu@DESKTOP-V3QJH6R>
2017-12-14 14:01:38 +0100

    correction classe Adherent

--- projet_11_ADHER/src/projet_11_adher/Adherents.java
+++ projet_11_ADHER/src/projet_11_adher/Adherents.java
@@ -5,20 +5,27 @@
  */
 package projet_11_adher;
 
+import java.util.Date;
+
 /**
  *
  * @author romain
  */
 public class Adherents {
-    private [String][nom];
-    private [String][prenom];
-    private [String][adresse];
-    private [Date][dateEntree];
-    private [Date][dateSortie];
-    private [Boolean][disponibilite]    
-}
-[Void][adherents]([String nom],[String prenom], [String adresse], [Date dateEntree], [Date dateSortie]){
-
-;
+    private String nom;
+    private String prenom;
+    private String adresse;
+    private Date dateEntree;
+    private Date dateSortie;
+    private Boolean disponibilite;
+    
+    public Adherents(String nom,String prenom, String adresse, Date dateEntree, Date dateSortie){
+        this.nom = nom;
+        this.prenom = prenom;
+        this.adresse = adresse;
+        this.dateEntree = dateEntree;
+        this.dateSortie = dateSortie;
+        this.disponibilite = true;
+    }
 }
 
