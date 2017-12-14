7e42874b7836fec2236f80517589443535178034
romain <romain@kali>
2017-12-14 13:55:54 +0100

    j'ai fait des classes

---
+++ projet_11_ADHER/src/projet_11_adher/Adher.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projet_11_adher;
+
+/**
+ *
+ * @author romain
+ */
+public class Adher {
+    [void] [adher](){
+    ;
+}
+    [void][supprimeAdherents()]{
+    ;
+}
+    [void][ajouteAdherents(adherent x)]{
+        ;
+    }
+    [void][renouvellementAdherents()]{
+        ;
+    }
+    [void][supprimeAppel()]{
+        ;
+    }
+    [void][ajouterAppel(appel x)]{
+        ;
+    }
+    [void][supprimeIntervention()]{
+        ;
+    }
+    [void][ajouterIntervention(intervention x)]{
+        ;
+    }
+    [void][modifierIntervention()]{
+        ;
+    }
+    [void][trouverIntervention()]{
+        ;
+    }
+}
