b12214d14c9791c2df56d2c582c4beb54f0ceb40
romain <romain@kali>
2017-12-14 13:52:01 +0100

    j'ai fait des classes

---
+++ projet_11_ADHER/src/projet_11_adher/Interventions.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projet_11_adher;
+
+/**
+ *
+ * @author romain
+ */
+public class Interventions {
+ private [Integer][num];
+ private [String][typeInteger];
+ [void][interventions(Integer Num, String Type)]{
+     ;
+ }
+}
